fa4386627b5f74393289a48a809a048806afe0a3
Philip Hayes <philiphayes9@gmail.com>
2023-09-19 19:27:23 -0700

    nix: hm: just link to dotfiles `tmux.conf`

--- home/mods/tmux.nix
+++ home/mods/tmux.nix
@@ -1,8 +1,12 @@
-{pkgs, ...}: {
+{config, pkgs, ...}:
+let
+  mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
+  dotfilesDir = config.home.dotfilesDir;
+in {
   home.packages = [
     pkgs.tmux
   ];
 
   # link tmux.conf
-  home.file.".tmux.conf".source = ../../tmux.conf;
+  home.file.".tmux.conf".source = mkOutOfStoreSymlink "${dotfilesDir}/tmux.conf";
 }
