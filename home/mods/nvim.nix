6bc176b24d9425c3f7165c7c2d62f52f24013fbd
Philip Hayes <philiphayes9@gmail.com>
2023-09-20 18:37:07 -0700

    nix: nvim: inject extra pkgs into nvim PATH w/o leaking into env

--- home/mods/nvim.nix
+++ home/mods/nvim.nix
@@ -1,31 +1,39 @@
+# A manually configured neovim w/o home-manager's config so I can get the nvim
+# config dir symlink working.
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
   mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
   dotfilesDir = config.home.dotfilesDir;
 
-  neovimConfig =
-    (pkgs.neovimUtils.makeNeovimConfig {
-      withNodeJs = true; # used by `coc.nvim` LSP client
-      withPython3 = false;
-      withRuby = false;
-    })
-    // {
-      # Args passed directly to `wrapNeovimUnstable`
+  # packages to add to the nvim PATH w/o infecting whole env
+  # TODO: mkOption
+  extraPkgs = [
+    # semi-functioning nix language server.
+    # maybe someday nix users won't have to live in fucking squalor...
+    pkgs.nil
+  ];
+  extraPkgsPath = lib.makeBinPath extraPkgs;
+
+  neovimConfigBase = pkgs.neovimUtils.makeNeovimConfig {
+    withNodeJs = true; # used by `coc.nvim` LSP client
+    withPython3 = false;
+    withRuby = false;
+  };
+
+  # Need some args passed directly to `wrapNeovimUnstable`
+  neovimConfig = neovimConfigBase // {
       # Don't manage config, we'll just symlink to our `dotfiles/nvim/init.vim`.
       wrapRc = false;
+      # Inject extra packages into nvim PATH.
+      wrapperArgs = neovimConfigBase.wrapperArgs ++ ["--suffix" "PATH" ":" extraPkgsPath];
     };
 in {
-  # Using a manually configured neovim w/o home-manager's config so I can get
-  # the nvim config dir symlink working.
-
   home.packages = [
     (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped neovimConfig)
-
-    # TODO: just add this to wrapped `nvim` PATH instead
-    pkgs.nil
   ];
 
   # symlink ~/.config/nvim to `dotfiles/nvim` dir.
