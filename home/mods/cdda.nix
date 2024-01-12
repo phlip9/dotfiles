012feeddf9922944571694ce552f35ad9145201e
Philip Hayes <philiphayes9@gmail.com>
2024-01-11 17:32:46 -0800

    cdda: 2023-12-28 -> 2024-01-11
    + Add Sky Islands (Milestone 1) update

--- home/mods/cdda.nix
+++ home/mods/cdda.nix
@@ -17,9 +17,9 @@
   };
 
   cdda-pre-fix = pkgs.cataclysm-dda-git.override {
-    version = "2023-12-28";
-    rev = "3f44fb3ebf533a3475e0a146b6bd22353e7c7b65";
-    sha256 = "sha256-IsNj+R24lBCO66ZdrJla7Vl4l5lsfKnGHVO6ljpZHoA=";
+    version = "2024-01-11";
+    rev = "c51a21b458e962d17f3bd322c38be0260a627f5e";
+    sha256 = "sha256-RTKMAN5UcuDtwXwXweu8TN/q9Es04j5FoNMy4Zltgcs=";
     useXdgDir = true;
   };
 
@@ -45,11 +45,24 @@
           hash = "sha256-X0da9cs60sr5jq4TPTMkNQAHAjcu1gPagJyLDJ7HOe0=";
         };
       };
+
+      # Remove this after upstream cdda repo gets the update
+      mod.Sky-Islands = cddaLib.buildMod {
+        modName = "Sky-Islands (Milestone 1)";
+        version = "0.3";
+        src = pkgs.fetchFromGitHub {
+          owner = "TGWeaver";
+          repo = "CDDA-Sky-Islands";
+          rev = "4d06105b77f69eb492e78c9aedf2ffdd15d1b42e";
+          hash = "sha256-MHTKoYqG4MhUFRqoNeB7X6HCd3rHRI0JL17jfkWE5e4=";
+        };
+      };
     };
 
   cdda = (cddaLib.attachPkgs cddaLib.pkgs cdda-no-mod).withMods (mods:
     with mods.extend customMods; [
       soundpack.CC-Sounds
+      mod.Sky-Islands
     ]);
 in {
   home.packages = [cdda];
