034cb59d278039fa968ffcaa4c9adbd426ceb17d
Philip Hayes <philiphayes9@gmail.com>
2024-03-14 16:54:11 -0700

    cdda: just use latest 0.G stable -- takes too long to compile

--- home/mods/cdda.nix
+++ home/mods/cdda.nix
@@ -16,45 +16,16 @@
       ;
   };
 
-  cdda-pre-fix = pkgs.cataclysm-dda-git.override {
-    version = "2024-01-11";
-    rev = "c51a21b458e962d17f3bd322c38be0260a627f5e";
-    sha256 = "sha256-RTKMAN5UcuDtwXwXweu8TN/q9Es04j5FoNMy4Zltgcs=";
-    useXdgDir = true;
-  };
-
-  cdda-no-mod = cdda-pre-fix.overrideAttrs (super: {
-    # patch doesn't cleanly apply anymore
-    patches = [];
-
-    passthru =
-      super.passthru
-      // {
-        pkgs = pkgs.override {build = cdda-no-mod;};
-        withMods = cddaLib.wrapCDDA cdda-no-mod;
-      };
-  });
+  cdda-no-mod = pkgs.cataclysm-dda;
 
   customMods = self: super:
     lib.recursiveUpdate super {
-      soundpack.CC-Sounds = cddaLib.buildSoundPack {
+      soundpack.CC-Sounds = cddaLib.buildSoundPack rec {
         modName = "CC-Sounds";
-        version = "2023-12-10";
+        version = "2024-01-17";
         src = pkgs.fetchzip {
-          url = "https://github.com/Fris0uman/CDDA-Soundpacks/releases/download/2023-12-10/CC-Sounds.zip";
-          hash = "sha256-X0da9cs60sr5jq4TPTMkNQAHAjcu1gPagJyLDJ7HOe0=";
-        };
-      };
-
-      # Remove this after upstream cdda repo gets the update
-      mod.Sky-Islands = cddaLib.buildMod {
-        modName = "Sky-Islands (Milestone 1)";
-        version = "0.3";
-        src = pkgs.fetchFromGitHub {
-          owner = "TGWeaver";
-          repo = "CDDA-Sky-Islands";
-          rev = "4d06105b77f69eb492e78c9aedf2ffdd15d1b42e";
-          hash = "sha256-MHTKoYqG4MhUFRqoNeB7X6HCd3rHRI0JL17jfkWE5e4=";
+          url = "https://github.com/Fris0uman/CDDA-Soundpacks/releases/download/${version}/CC-Sounds.zip";
+          hash = "sha256-IkqdUZyfK50YV4dHapEjjBm/SVhOjZk0jiMKCo5h7gQ=";
         };
       };
     };
@@ -62,7 +33,6 @@
   cdda = (cddaLib.attachPkgs cddaLib.pkgs cdda-no-mod).withMods (mods:
     with mods.extend customMods; [
       soundpack.CC-Sounds
-      mod.Sky-Islands
     ]);
 in {
   home.packages = [cdda];
