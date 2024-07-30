156c1cdd7ee339c4dd2f1b481381579b3c4d3ae2
Philip Hayes <philiphayes9@gmail.com>
2024-07-30 14:59:05 -0700

    nix: update nixpkgs & home-manager -> 24.05
    • Updated input 'home-manager':
        'github:nix-community/home-manager/67de98ae6eed5ad6f91b1142356d71a87ba97f21' (2024-04-22)
      → 'github:nix-community/home-manager/e1391fb22e18a36f57e6999c7a9f966dc80ac073' (2024-07-03)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/572af610f6151fd41c212f897c71f7056e3fb518' (2024-04-23)
      → 'github:nixos/nixpkgs/12bf09802d77264e441f48e25459c10c93eada2e' (2024-07-29)

--- flake.nix
+++ flake.nix
@@ -36,10 +36,10 @@
   # };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
