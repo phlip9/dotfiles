87b8d836c1548bf631ef771d815286455867bcbf
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-01-30 16:24:33 -0800

    nix: update `samply` 1cb135d8 (20241210) -> 52e453d3 (20250124)

--- pkgs/samply.nix
+++ pkgs/samply.nix
@@ -1,19 +1,20 @@
+# `samply` - command-line sampling profiler for macOS and Linux
 {
   rustPlatform,
   fetchFromGitHub,
 }:
 rustPlatform.buildRustPackage {
   pname = "samply";
-  version = "0.13.1-unstable-20241210";
+  version = "0.13.1-unstable-20250124";
 
   src = fetchFromGitHub {
     owner = "mstange";
     repo = "samply";
-    rev = "1cb135d801c4f1a977b7035d04629891f2eb8b6b";
-    hash = "sha256-rpVPvzHnCZCnKDGdFKLAQtrzK1BuP+eGjod67L0pNzU=";
+    rev = "52e453d3df1ea1f52005897b7887576be4a129ae";
+    hash = "sha256-LEJL3t2ceT5QPZodY/9YupxqX8L/pT0L2znwzgNyn68=";
   };
 
-  cargoHash = "sha256-tH8uy8okQ43JRs13WgKuzkp5Mjkbx6wBXczJZ0Ek5mw=";
+  cargoHash = "sha256-pwU9axN0qAhqmyk04vDmu+YScA5ocqYbTASqjw9o7jE=";
 
   cargoBuildFlags = "-p samply --bin samply";
 }
