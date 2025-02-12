fbbbae6981150389f3524c60c66d367801bc971f
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-02-11 16:46:52 -0800

    nix: claude: configure filesystem MCP server

--- home/mods/claude.nix
+++ home/mods/claude.nix
@@ -1,8 +1,26 @@
 # Anthropic Claude desktop and extensions
-{phlipPkgs, ...}: {
-  # TODO(phlip9): Claude desktop
-  home.packages = [
-    # a MCP provider for local filesystem access
-    phlipPkgs.mcp-server-filesystem
-  ];
+{
+  config,
+  phlipPkgs,
+  pkgs,
+  ...
+}: {
+  # Claude Desktop config
+  home.file."Library/Application Support/Claude/claude_desktop_config.json" = {
+    enable = pkgs.hostPlatform.isDarwin;
+    source = pkgs.writers.writeJSON "claude_desktop_config.json" {
+      mcpServers = {
+        # fs access (with approval ofc)
+        filesystem = {
+          command = "${phlipPkgs.mcp-server-filesystem}/bin/mcp-server-filesystem";
+          # args: list of dirs the agent can access _only after approval_. The
+          # agent can't access anything outside these dirs; the chat UI won't
+          # even ask for approval.
+          args = [
+            "${config.home.homeDirectory}/dev"
+          ];
+        };
+      };
+    };
+  };
 }
