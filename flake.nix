9907d4ca218c76e9aaa34334edbb0b17bc05fe96
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-02-13 13:05:21 -0800

    nix: update flake.lock
    • Updated input 'home-manager':
        'github:nix-community/home-manager/10e99c43cdf4a0713b4e81d90691d22c6a58bdf2' (2024-12-28)
      → 'github:nix-community/home-manager/bd65bc3cde04c16755955630b344bc9e35272c56' (2025-01-08)
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/634fd46801442d760e09493a794c4f15db2d0cbb' (2024-12-27)
      → 'github:nixos/nixpkgs/efc60b3e477818e3b5fcec5f3a7c0aea4865ae87' (2025-02-12)

--- flake.nix
+++ flake.nix
@@ -36,12 +36,12 @@
   # };
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-      # url = "github:nix-community/home-manager/release-24.05";
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
+      # url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
