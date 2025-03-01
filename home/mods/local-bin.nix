dcb7df74158ee57ec8a576f0481fa771df8745fc
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-03-01 10:25:43 -0800

    nix: use out-of-store symlink for ~/.local/bin entries

--- home/mods/local-bin.nix
+++ home/mods/local-bin.nix
@@ -1,9 +1,16 @@
 # random scripts we link into ~/.local/bin from dotfiles/bin
-{pkgs, ...}: {
-  home.file.".local/bin/picolispfmt".source = ../../bin/picolispfmt;
-
+{
+  pkgs,
+  config,
+  ...
+}: let
+  dotfilesDir = config.home.dotfilesDir;
+  hostPlatform = pkgs.hostPlatform;
+  mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink;
+in {
+  home.file.".local/bin/picolispfmt".source = mkOutOfStoreSymlink "${dotfilesDir}/bin/picolispfmt";
   home.file.".local/bin/traceexec.d" = {
-    enable = pkgs.stdenv.isDarwin;
-    source = ../../bin/traceexec.d;
+    enable = hostPlatform.isDarwin;
+    source = mkOutOfStoreSymlink "${dotfilesDir}/traceexec.d";
   };
 }
