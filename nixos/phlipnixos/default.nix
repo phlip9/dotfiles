03a659c0aa8520b1a5298f947f547dfe15c46c32
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-10-26 16:06:35 -0700

    nixos: import phlipnixos configuration.nix after iso install

---
+++ nixos/phlipnixos/default.nix
@@ -0,0 +1,157 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # use latest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "phlipnixos";
+  networking.wireless.enable = false;
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/Los_Angeles";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  #
+  # Nix
+  #
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  #
+  # Window/Display Manager
+  #
+
+  # enable X11 windowing system
+  services.xserver.enable = true;
+
+  # enable GNOME desktop environment
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # disable CUPS. don't need printing.
+  services.printing.enable = false;
+
+  # enable sound with pipewire
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  #
+  # Nvidia graphics drivers
+  #
+
+  # enable hardware acceleration
+  hardware.graphics.enable = true;
+  # load nvidia drivers for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  nixpkgs.config.nvidia.acceptLicense = true;
+
+  hardware.nvidia = {
+    # use proprietary drivers. OSS drivers only support Turing+ (1080 Ti /
+    # Pascal is too old)
+    open = false;
+
+    # stable (v570.153.02) and beta (575.64.05) fail to build against kernel v6.17.0
+    # package = config.boot.kernelPackages.nvidiaPackages.beta;
+    # <https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/linux/nvidia-x11/default.nix#L74>
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "580.95.05";
+      sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
+      sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
+      openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
+      settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
+      persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
+    };
+  };
+
+  #
+  # User
+  #
+
+  users.users.phlip9 = {
+    isNormalUser = true;
+    description = "Philip Kannegaard Hayes";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = [ ];
+  };
+
+  # install firefox
+  programs.firefox.enable = true;
+
+  # allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # list packages installed in system profile
+  environment.systemPackages = [
+    pkgs.vim
+    pkgs.wget
+  ];
+
+  # enable 1Password
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "phlip9" ];
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+}
---
+++ nixos/phlipnixos/hardware-configuration.nix
@@ -0,0 +1,71 @@
+{
+  config,
+  lib,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
+
+  boot.initrd.availableKernelModules = [
+    "xhci_pci"
+    "ahci"
+    "nvme"
+    "usb_storage"
+    "usbhid"
+    "sd_mod"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  boot.initrd.luks.devices = {
+    "crypt-nixos".device = "/dev/disk/by-uuid/809d56ca-d278-49e7-845b-47f4354ea6a1";
+    "crypt-swap".device = "/dev/disk/by-uuid/817d77e0-f4fd-43c2-8307-4ab50caf50d8";
+    "crypt-phlipdisk3".device =
+      "/dev/disk/by-uuid/74a62a74-f99b-4feb-8d6f-9bd76140b4fb";
+    "crypt-ubuntu".device =
+      "/dev/disk/by-uuid/bcaf844a-8cfb-4cd4-bff0-10d68b252a3e";
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/65293450-d106-4e73-8617-79477f8be423";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/63F1-F7B4";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+    "/mnt/phlipdisk3" = {
+      device = "/dev/disk/by-uuid/0b5829aa-d02f-4a0d-9022-32bb83b6a7a2";
+      fsType = "ext4";
+      options = [ "ro" ];
+    };
+    "/mnt/ubuntu" = {
+      device = "/dev/disk/by-uuid/3a888929-866b-4f92-b4d2-7030c295c441";
+      fsType = "ext4";
+      options = [ "ro" ];
+    };
+  };
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/c0d029d9-610d-4be3-a4ae-e88256dddbb6"; }
+  ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
