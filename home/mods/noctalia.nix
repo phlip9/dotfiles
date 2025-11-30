a29274624c642eb0bd107ec9813f2afdb340a378
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-11-29 20:11:57 -0800

    hm: point to noctalia generated themes

--- home/mods/noctalia.nix
+++ home/mods/noctalia.nix
@@ -6,4 +6,16 @@ in
 {
   xdg.configFile."noctalia".source =
     mkOutOfStoreSymlink "${dotfilesDir}/config/noctalia";
+
+  xdg.configFile."gtk-3.0/gtk.css".source =
+    mkOutOfStoreSymlink "${dotfilesDir}/config/gtk-3.0/gtk.css";
+  xdg.configFile."gtk-4.0/gtk.css".source =
+    mkOutOfStoreSymlink "${dotfilesDir}/config/gtk-4.0/gtk.css";
+  xdg.configFile."qt5ct/colors/noctalia.conf".source =
+    mkOutOfStoreSymlink "${dotfilesDir}/config/qt5ct/colors/noctalia.conf";
+  xdg.configFile."qt6ct/colors/noctalia.conf".source =
+    mkOutOfStoreSymlink "${dotfilesDir}/config/qt6ct/colors/noctalia.conf";
+
+  xdg.configFile."fuzzel/themes/noctalia".source =
+    mkOutOfStoreSymlink "${dotfilesDir}/config/fuzzel/themes/noctalia";
 }
