47b5ca2efe41055477fc35f9ef21506265a40489
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-12-08 23:29:46 -0800

    firefox: pin Sort Tabs

--- nixos/phlipnixos/default.nix
+++ nixos/phlipnixos/default.nix
@@ -160,6 +160,11 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/ctrl-number-to-switch-tabs/latest.xpi";
           installation_mode = "normal_installed";
         };
+        # Sort Tabs
+        "{cd89151d-78f2-4f57-9a91-52c0738028f2}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/sort-tabs-00/latest.xpi";
+          installation_mode = "normal_installed";
+        };
         # 1Password
         "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
