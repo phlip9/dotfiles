93e6b34562fb6e9681efed14eba7ca604b8bddb7
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2025-12-16 15:58:23 -0800

    codex: 0.63.0 -> 0.72.0

--- pkgs/codex.nix
+++ pkgs/codex.nix
@@ -6,22 +6,22 @@
   nix-update-script,
 }:
 let
-  version = "0.63.0";
+  version = "0.72.0";
 
   # ```bash
-  # $ (export version="0.63.0" target="aarch64-apple-darwin";
+  # $ (export version="0.72.0" target="x86_64-unknown-linux-gnu";
   #    nix store prefetch-file "https://github.com/openai/codex/releases/download/rust-v${version}/codex-${target}.tar.gz";)
   # ```
   sources = {
     x86_64-linux = rec {
       target = "x86_64-unknown-linux-gnu";
       url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-${target}.tar.gz";
-      hash = "sha256-15CH4Bit+i7TZgPPCq5WdqT/rVuHGcYi27FbNKfJZ2s=";
+      hash = "sha256-oOSVzMCP0dE37CFN9Wx1Mg5S0Hf9k6i1T0UwkUK8Xh8=";
     };
     aarch64-darwin = rec {
       target = "aarch64-apple-darwin";
       url = "https://github.com/openai/codex/releases/download/rust-v${version}/codex-${target}.tar.gz";
-      hash = "sha256-p6Zsj+EyxWNM3/zLefO7Z+p9SH2dEFLoGPsLpWp+HoY=";
+      hash = "sha256-+ce2hx8FClfX5owsAZY8DjTEBCuZ8YDs35UNybGsevg=";
     };
   };
 
