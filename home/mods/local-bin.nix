f79cc9fd327fa84558df7cd3e6c5a6aa938fb337
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2026-02-16 14:19:33 +0800

    hm: add signal china workaround

--- home/mods/local-bin.nix
+++ home/mods/local-bin.nix
@@ -15,6 +15,8 @@ in
     ".local/bin/nor".source = mkOutOfStoreSymlink "${dotfilesDir}/bin/nor";
     ".local/bin/picolispfmt".source =
       mkOutOfStoreSymlink "${dotfilesDir}/bin/picolispfmt";
+    ".local/bin/signal-china".source =
+      mkOutOfStoreSymlink "${dotfilesDir}/bin/signa-china";
     ".local/bin/traceexec.d" = {
       enable = hostPlatform.isDarwin;
       source = mkOutOfStoreSymlink "${dotfilesDir}/bin/traceexec.d";
