16790e80b97d0409877d8217ceb2b21659ef41b2
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2026-03-07 21:02:32 -0800

    style: gen "dunes" colors

--- just/wallpaper-colors.sh
+++ just/wallpaper-colors.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 wallpaper_name="${1:-default}"
-repo_root="$PWD"
 
 build_out=$(
   nix build -f . --no-link --print-out-paths \
@@ -14,11 +13,9 @@ if [[ ! -d "$build_out/config" ]]; then
   exit 1
 fi
 
-# Copy the generated config payload into the repo config/ tree.
+# Copy the generated configs into the repo
 fd . \
   --base-directory "$build_out/config" \
   --strip-cwd-prefix=always \
   --type file \
-  --exec bash -c \
-  "install -D --mode=0644 -- \"\$1\" \"\$2/config/\$1\"" \
-  bash {} "$repo_root"
+  --exec install -D --mode=0644 -- "{}" "$PWD/config/{}"
